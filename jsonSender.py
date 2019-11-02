73c41f8a9e98095701fe1402f123003b42ea3ff8
BananaRaptor <arnaudthebergemandeville@gmail.com>
2019-11-02 19:09:53 -0400

    test 1

--- jsonSender.py
+++ jsonSender.py
@@ -1,23 +1,26 @@
 import json
 import requests
 
-var1 = 1
-var2 = 2
-exp = 3
-res = 14
+
+def getData():
+
+    var1 = 3
+    min1 = 1
+    max1 = 5
+    var2 = 2
+    min2 = 1
+    max2 = 5
+    res = 14
+    jsonOut(var1, min1, max1, var2, min2, max2, res)
 
 
-def jsonOut():
-    jsonTest = json.dumps(['data', {'input': (var1, var2)}, {'expected': exp}, {'got': res}])
+def jsonOut(value1, min1, max1, value2, min2, max2, res):
+    jsonTest = json.dumps(
+        ['data', {'variable1': (value1, min1, max1)}, {'variable2': (value2, min2, max2)}, {'got': res}])
     response = requests.get('http://wolflo.net:6650')
-    requests.post('http://wolflo.net:6650', jsonTest)
     print response
+    requests.post('http://wolflo.net:6650', jsonTest)
+
+
 if __name__ == '__main__':
-    jsonOut()
-
-
-
-
-
-
-
+    getData()
